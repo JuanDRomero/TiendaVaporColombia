7408a94a2dd50791646b609ab7770c50120d53a2
JuanTabaresM <jjtabaresm@uqvirtual.edu.co>
2021-05-08 21:19:31 -0500

    Primera version

---
+++ back-end/venv/Scripts/vaporc/api/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from rest_framework import permissions
+from .serializers import MovieSerializer, GroupSerializer
+from .models import Movie
+
+
+class MovieViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
